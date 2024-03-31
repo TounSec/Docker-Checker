82ba15e585ff5a193e2984e9bdec648ae5ace1da
pwnsec <pwnsec@reverse-sec.lan>
2024-03-31 19:05:53 -0400

    Init

---
+++ docker_checker.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# Help
+if [ $# -lt 1 ] || [ $1 == "-h" ] || [ $1 == "--help" ]; then
+	echo -e "\033[34mAuto Vulnerability Scan for docker images with\033[33m GRYPE\033[0m"
+	echo ""
+	echo "-h, --help						  Print help"
+	echo "[MODES]"
+	echo "-ai, --all-image				    Scan all installed docker image for vulnerabilities "
+	echo "-ac, --all-container	      			Scan  all containers for image vulnerabilities"
+	echo ""
+	echo "[OPTIONS :]"
+	echo "-v, --verbose					      Detail of the output scan execution"
+	echo "-o, --output						Path output for each files result of the scan, if the directory doesn't exist, he is created"
+	echo ""
+	echo "Usage : ./docker_image_checker.sh [MODES] [OPTIONS]"
+	exit
+fi
+
+# Arguments management
+MODE=""
+VERBOSE=false
+OUTPUT=""
+while (($#)); do
+	case $1 in
+		-ai|--all-image)
+			MODE="--all-image"
+			shift
+			;;
+		-ac|--all-container)
+			MODE="--all-container"
+			shift
+			;;
+		-v|--verbose)
+			VERBOSE=true
+			shift
+			;;
+		-o|--output)
+			if [ -z $2 ] || [ ${2:0:1} == "-" ]; then
+				echo "Output path is missing or invalid after -o|--output option"
+				exit 1
+			fi
+			OUTPUT="$2"
+			shift 2
+			;;
+		*)
+			echo "Unrecognized argument : $arg"
+			exit 1
+			;;
+	esac
+done
+
+# Check if the output directory exist and create it if it doesn't
+if [ ! -z $OUTPUT ]; then
+	mkdir -p $OUTPUT
+fi
+
+# Quick scan function for image vulnerabilities with GRYPE
+vuln_scan() {
+	local IMAGES=$1
+	local VERBOSE=$2
+	local OUTPUT=$3
+	
+	if [ $VERBOSE == true  ]; then
+		if [ ! -z $OUTPUT ]; then
+			for IMAGE in $IMAGES; do
+				echo -e "\033[34m[$(date -u +"%Y-%m-%d %H:%M UTC")]\033[0m Quick Vulnerability Scan for \033[33m$IMAGE\033[0m"
+				TIME_START=$(date +%s)
+				grype $IMAGE | tee "$OUTPUT/$IMAGE.txt"
+				echo ""
+			done
+		else
+			for IMAGE in $IMAGES; do	
+				echo -e "\033[34m[$(date -u +"%Y-%m-%d %H:%M UTC")]\033[0m Quick Vulnerability Scan for \033[33m$IMAGE\033[0m"
+				TIME_START=$(date +%s)
+				grype $IMAGE
+				echo ""
+			done
+		fi
+			TIME_DIFF=$[ $(date +%s) - $TIME_START ]
+			echo -e "Time to scan : \033[36m$TIME_DIFF secondes\033[0m"		
+	else 
+		if [ ! -z $OUTPUT ]; then
+			for IMAGE in $IMAGES; do
+				echo -e "\033[34mQuick Vulnerability Scan for \033[33m$IMAGE\033[0m"
+				grype $IMAGE | tee "$OUTPUT/$IMAGE.txt"
+				echo ""
+			done
+		else
+			for IMAGE in $IMAGES; do
+				echo -e "\033[34mQuick Vulnerability Scan for \033[33m$IMAGE\033[0m"
+				grype $IMAGE
+				echo ""
+			done
+		fi	
+	fi
+}
+
+# Scan for all container image
+if [ $MODE == "--all-container" ]; then
+	IMAGES=$(sudo docker ps -a --format "{{.Image}}")
+	vuln_scan "$IMAGES" "$VERBOSE" "$OUTPUT"
+
+# Scan for all installed docker image
+elif [ $MODE == "--all-image" ]; then
+	IMAGES=$(sudo docker images --format "{{.Repository}}:{{.Tag}}")
+	vuln_scan "$IMAGES" "$VERBOSE" "$OUTPUT"
+fi
